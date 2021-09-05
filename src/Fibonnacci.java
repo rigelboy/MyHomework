dff1eb6c88d7aeb026e13f95d33e9c66a27b1b3d
cansuqi <528682902@qq.com>
2021-09-05 19:54:23 +0800

    斐波那契数列作业提交

--- src/Fibonnacci.java
+++ src/Fibonnacci.java
@@ -1,13 +1,13 @@
 public class Fibonnacci {
     public static void main(String[] arr){
-        int axx[] = new int[10];
+        int[] aar = new int[10];
         int i=2;
-        axx[0]=0;
-        axx[1]=1;
-        System.out.println(axx[1]+"");
+        aar[0]=0;
+        aar[1]=1;
+        System.out.println(aar[1]+"");
         while(i<10){
-            axx[i]=axx[i-1]+axx[i-2];
-            System.out.println(axx[i]+"");
+            aar[i]=aar[i-1]+aar[i-2];
+            System.out.println(aar[i]+"");
             i++;
         }
         System.out.println(" ");
