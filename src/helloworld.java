a2759171d6eb5365d76931cfb6de79b68059093f
cansuqi <528682902@qq.com>
2021-09-05 16:42:43 +0800

    6个hello world

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main(String[] arr){
+        System.out.print(" HELLO WORLD");
+        System.out.print("HELLO WORLD");
+        System.out.print("hello World");
+        System.out.print("hello world");
+        System.out.print("hello world");
+        System.out.print("hello world");
+    }
+}
