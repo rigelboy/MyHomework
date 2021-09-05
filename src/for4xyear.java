ffa983489face8a008918b292a16fbe02ca77303
cansuqi <528682902@qq.com>
2021-09-05 18:41:44 +0800

    闰年作业提交

---
+++ src/for4xyear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class for4xyear {
+    public static void main(String[] arr){
+        int num=0;
+        int year;
+        Scanner s =new Scanner(System.in);
+        System.out.println("请输入年份");
+        year=s .nextInt();
+        System.out.println(year);
+        for(int i=year;i<=year+100;i++)
+        {
+            // 可以被4整除，不被100整除    或者     可以被400整除的是闰年
+            if((i%4==0&&i%100!=0)||(i%400==0)){
+                num++;
+                if(num%2==0)
+                {
+                    System.out.println(i);
+                }
+                else{
+                    System.out.print(i+" ");
+                }
+            }
+        }
+
+    }
+}
