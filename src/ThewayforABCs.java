92e0a1d944ea43b56340767e0d6029dc6e49e0be
cansuqi <528682902@qq.com>
2021-09-07 23:11:42 +0800

    字符数组作业提交

--- src/ThewayforABCs.java
+++ src/ThewayforABCs.java
@@ -1,10 +1,15 @@
-import java.util.Scanner;
+public class ThewayforABCs {
+    public static void getNums(String str) {
+        int[] nums = new int[26];
 
-public class ThewayforABCs{
-    public static void main(String[] args){
-        Scanner s =new Scanner(System.in);
-        String a;
-        a = s.next();
-        System.out.println(a);
-    }
-}
+        for(char i : str.toCharArray()) {
+            if(i >= 97 && i <= 123) {
+                nums[i - 97]++;
+            }
+        }
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != 0) {
+                char j = (char) (i + 97);
+                System.out.println(j + "出现次数为" + nums[i]);
+            }}}}
+
