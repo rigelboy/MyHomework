c94c373ad12d66a6040a583e9a2aa89742be070b
cansuqi <528682902@qq.com>
2021-09-07 18:23:23 +0800

    数组平均作业提交

---
+++ src/ThewayforABCs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ThewayforABCs{
+    public static void main(String[] args){
+        Scanner s =new Scanner(System.in);
+        String a;
+        a = s.next();
+        System.out.println(a);
+    }
+}
